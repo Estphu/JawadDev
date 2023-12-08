42d0ad45634c8aefc3e78c758e2aa5b11b8fa663
Estphu <jaydee20001217@gmail.com>
2023-12-08 23:15:51 +0500

    API - Customization&Styling in api.html

--- jawaddev/api/urls.py
+++ jawaddev/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostReadOnlyApiView
+from .views import PostAPIView
 
 app_name = 'api'
 
 router = DefaultRouter()
 
-router.register(r'posts', PostReadOnlyApiView, basename='post')
+router.register(r'posts', PostAPIView, basename='post')
 
 urlpatterns = [
     path('blog/',include(router.urls))
--- jawaddev/api/views.py
+++ jawaddev/api/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from rest_framework.generics import ListAPIView
 from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer
 from blog.models import Post
 from .serializers import PostSerializer
 
-# Create your views here.
-class PostReadOnlyApiView(viewsets.ReadOnlyModelViewSet):
+class PostAPIView(viewsets.ReadOnlyModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'rest_framework/api.html'
