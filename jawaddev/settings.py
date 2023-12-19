874b1f4372a47702dac651bfbdf51ba2adf5e745
Estphu <jaydee20001217@gmail.com>
2023-12-19 15:32:42 +0500

    Deployment - Railway - Content-Updated

--- jawaddev/settings.py
+++ jawaddev/settings.py
@@ -34,6 +34,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
+# jawaddev-production.up.railway.app
+
 ALLOWED_HOSTS = ['jawaddev-production.up.railway.app']
 
 # Application definition
