d0de2ddf1c03d1839531f1b82ed0788902dfb1bc
Estphu <jaydee20001217@gmail.com>
2024-01-09 02:16:39 +0500

    Scraping: Pakistan 2018 Election Results Added

--- scraping/views.py
+++ scraping/views.py
@@ -21,7 +21,7 @@ def elections_result(request):
     # Set the path to Chromedriver in the options
     # chrome_options.add_argument('executable_path=/bin/chromedriver.exe')
 
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))  # You might need to adjust the path or use another driver
+    driver = webdriver.Chrome(service=ChromiumService(executable_path=ChromeDriverManager().install()))  # You might need to adjust the path or use another driver
 
     try:
     # Load the page
